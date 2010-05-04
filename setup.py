0206c25e10c964c8a9b8eb28670f40247317891c
Manuel Aristarán <jazzido@jazzido.com>
2010-05-04 15:22:59 -0300

    included templates in package_data

--- setup.py
+++ setup.py
@@ -1,17 +1,27 @@
 from distutils.core import setup
 
 setup(name='userprofile',
-        version='0.6',
-        description='Django pluggable user profile zone',
-        author='David Rubert',
-        packages=['userprofile',
-            'userprofile.templatetags',
-            'userprofile.urls'],
-        classifiers=['Development Status :: 4 - Beta',
-            'Environment :: Web Environment',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: MIT License',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Topic :: Utilities'],
-        )
+      version='0.6',
+      description='Django pluggable user profile zone',
+      author='David Rubert',
+      packages=['userprofile',
+                'userprofile.templatetags',
+                'userprofile.urls'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+      package_data = {
+        'userprofile' : [ 'templates/userprofile/*.html', 
+                          'templates/userprofile/account/*.html', 
+                          'templates/userprofile/account/includes/*.html', 
+                          'templates/userprofile/avatar/*.html', 
+                          'templates/userprofile/email/*.txt', 
+                          'templates/userprofile/profile/*.html', 
+                          'locale/*/*/*', ]
+        },
+)
+
